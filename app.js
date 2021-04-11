cccbf9cecd369b40f1017fe014bc4c9f2f621497
Veronika Stuchlíková <stuchlikova.veronika@post.cz>
2021-04-11 17:33:04 +0200

    Hod kostkou

--- app.js
+++ app.js
@@ -10,8 +10,17 @@ document.getElementById("total_score_player0").textContent = 0;
 document.getElementById("total_score_player1").textContent = 0;
 document.getElementById("current_score0").textContent = 0;
 document.getElementById("current_score1").textContent = 0;
-
 document.querySelector(".dice_img").style.display = "none";
 
+// Po kliku na "Hodit kostkou" se objeví náhodná kostka
 
+document.querySelector(".rollDice").addEventListener('click', function() {
+    // generace náhodného čísla 1-6
+    let dice = Math.ceil(Math.random()*6);
 
+    // Zobrazení správného obrázku
+    let diceElement = document.querySelector(".dice_img");
+    diceElement.style.display = "block";
+    diceElement.src = "img/" + dice + ".jpg";
+    
+});
