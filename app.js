4063077f22d6ea1b3fc1aec9b3f3bedc0378d5f9
Veronika Stuchlíková <stuchlikova.veronika@post.cz>
2021-04-11 17:44:08 +0200

    Round score

--- app.js
+++ app.js
@@ -12,15 +12,24 @@ document.getElementById("current_score0").textContent = 0;
 document.getElementById("current_score1").textContent = 0;
 document.querySelector(".dice_img").style.display = "none";
 
-// Po kliku na "Hodit kostkou" se objeví náhodná kostka
 
-document.querySelector(".rollDice").addEventListener('click', function() {
+    // Po kliku na "Hodit kostkou" se objeví náhodná kostka
+    document.querySelector(".rollDice").addEventListener('click', function() {
     // generace náhodného čísla 1-6
     let dice = Math.ceil(Math.random()*6);
-
     // Zobrazení správného obrázku
     let diceElement = document.querySelector(".dice_img");
     diceElement.style.display = "block";
     diceElement.src = "img/" + dice + ".jpg";
     
+    // Načítání "současného skóre"(roundScore)
+    if (dice !== 1) {
+        roundScore = roundScore + dice;
+        document.getElementById("current_score0").textContent = roundScore;
+    } else {
+       // bude hrát další hráč
+    }
+   
+
 });
+
